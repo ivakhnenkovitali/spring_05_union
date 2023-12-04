af957817254bd2c29575dc508223bb4900d89127
Asus <vaxa2320@gmail.com>
2023-12-04 18:14:26 +0300

    12345678

--- src/main/java/by/IT/configs/AppConfig.java
+++ src/main/java/by/IT/configs/AppConfig.java
@@ -2,11 +2,14 @@ package by.IT.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @EnableWebMvc
 //@ComponentScan(basePackages = {"by.itclass.controllers", "by.itclass.model.repositories"})
-@ComponentScan("by.itclass")
+@ComponentScan("by.IT")
 public class AppConfig {
     @Bean
     public ViewResolver viewResolver() {
