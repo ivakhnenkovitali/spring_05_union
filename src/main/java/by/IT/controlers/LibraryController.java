8d02fc3a16313a219b09642467a791dc44231c37
Asus <vaxa2320@gmail.com>
2023-12-13 18:12:16 +0300

    1234567

--- src/main/java/by/IT/controlers/LibraryController.java
+++ src/main/java/by/IT/controlers/LibraryController.java
@@ -1,7 +1,7 @@
 package by.IT.controlers;
 
-
-import by.itclass.model.repositories.LibraryRepository;
+import by.IT.model.repositories.LibraryRepository;
+import by.repositories.LibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
